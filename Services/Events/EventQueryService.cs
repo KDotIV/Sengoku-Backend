71443cede509147ab9e8e2165ae3af8bfb35dda8
K. <126347036+FiendPopsicle@users.noreply.github.com>
2024-04-20 12:11:11 -0400

    + Optimized implementation to utilize region_ids to give faster/accurate search results

--- Services/Events/EventQueryService.cs
+++ Services/Events/EventQueryService.cs
@@ -1,5 +1,4 @@
-﻿using Dapper;
-using Npgsql;
+﻿using Npgsql;
 using SengokuProvider.API.Models.Events;
 using SengokuProvider.API.Models.Regions;
 using SengokuProvider.API.Services.Common;
@@ -15,20 +14,114 @@ namespace SengokuProvider.API.Services.Events
             _connectionString = connectionString;
             _validator = validator;
         }
+        public async Task<List<int>> QueryRelatedRegionsById(int regionId)
+        {
+            try
+            {
+                var relatedRegions = new List<int>();
 
+                using (var conn = new NpgsqlConnection(_connectionString))
+                {
+                    await conn.OpenAsync();
+
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = @"SELECT r2.id
+                            FROM public.regions r1
+                            JOIN regions r2 ON r1.province = r2.province
+                            WHERE r1.id = @InputRegionId
+                              AND r2.id != @InputRegionId;";
+
+                        cmd.Parameters.AddWithValue("@InputRegionId", regionId);
+
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                relatedRegions.Add(reader.GetInt32(0));
+                            }
+                        }
+                    }
+                }
+                relatedRegions.Add(regionId);
+                return relatedRegions;
+            }
+            catch (NpgsqlException ex)
+            {
+                throw new ApplicationException("Database error occurred: ", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Unexpected Error Occurred: ", ex);
+            }
+        }
+        public async Task<List<RegionData>> GetRegionData(List<int> regionIds)
+        {
+            var regions = new List<RegionData>();
+            try
+            {
+                using (var conn = new NpgsqlConnection(_connectionString))
+                {
+                    await conn.OpenAsync();
+                    using (var cmd = new NpgsqlCommand("SELECT id, name, latitude, longitude, province FROM regions WHERE id = ANY(@Input)", conn))
+                    {
+                        // Passing the list as an array parameter
+                        var param = new NpgsqlParameter("@Input", NpgsqlTypes.NpgsqlDbType.Array | NpgsqlTypes.NpgsqlDbType.Integer)
+                        {
+                            Value = regionIds.ToArray()
+                        };
+                        cmd.Parameters.Add(param);
+
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                regions.Add(new RegionData
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("id")),
+                                    Name = reader.GetString(reader.GetOrdinal("name")),
+                                    Latitude = reader.GetDouble(reader.GetOrdinal("latitude")),
+                                    Longitude = reader.GetDouble(reader.GetOrdinal("longitude")),
+                                    Province = reader.GetString(reader.GetOrdinal("province"))
+                                });
+                            }
+                        }
+                    }
+
+                    return regions;
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw new ApplicationException("Database error occurred: ", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Unexpected Error Occurred: ", ex);
+            }
+        }
         public async Task<List<AddressEventResult>> QueryEventsByLocation(GetTournamentsByLocationCommand command, int pageNumber)
         {
             if (command == null || command.RegionId == 0) throw new ArgumentNullException(nameof(command));
             try
             {
+                var currentRegions = await QueryRelatedRegionsById(command.RegionId);
+
                 var sortedAddresses = new List<AddressEventResult>();
 
                 using (var conn = new NpgsqlConnection(_connectionString))
                 {
                     await conn.OpenAsync();
 
-                    var newQuery = @"SELECT name, latitude, longitude, province FROM regions WHERE id = @Input";
-                    var regionResult = await conn.QueryFirstOrDefaultAsync<RegionData>(newQuery, new { Input = command.RegionId });
+                    var regionData = await GetRegionData(currentRegions);
+                    var regionIds = new List<int>();
+                    var locationReference = regionData.FirstOrDefault(x => x.Id == command.RegionId);
+
+                    foreach (var region in regionData)
+                    {
+                        regionIds.Add(region.Id);
+                    }
 
                     using (var cmd = new NpgsqlCommand())
                     {
@@ -38,21 +131,29 @@ namespace SengokuProvider.API.Services.Events
                                 a.address, a.latitude, a.longitude, 
                                 e.event_name, e.event_description, e.region, e.start_time, e.end_time, e.link_id,
                                 SQRT(
-                                    POW(a.longitude - @startLongitude, 2) + POW(a.latitude - @startLatitude, 2)
-                                ) as distance
+                                    POW(a.longitude - @ReferenceLongitude, 2) + POW(a.latitude - @ReferenceLatitude, 2)
+                                ) AS distance
                             FROM 
-                                addresses a
+                                events e
                             JOIN 
-                                events e ON a.id = e.address_id
+                                addresses a ON e.address_id = a.id
                             WHERE
-                                e.start_time >= CURRENT_DATE
+                                e.region = ANY(@RegionIds)
+                                AND e.start_time >= CURRENT_DATE
                             ORDER BY
                                 e.start_time ASC,
                                 distance ASC
-                            LIMIT @perPage;";
-                        cmd.Parameters.AddWithValue("@startLatitude", regionResult.Latitude);
-                        cmd.Parameters.AddWithValue("@startLongitude", regionResult.Longitude);
-                        cmd.Parameters.AddWithValue("@perPage", command.PerPage);
+                            LIMIT @PerPage;";
+
+                        var regionIdsParam = cmd.CreateParameter();
+                        regionIdsParam.ParameterName = "RegionIds";
+                        regionIdsParam.Value = regionIds.ToArray();
+                        regionIdsParam.NpgsqlDbType = NpgsqlTypes.NpgsqlDbType.Array | NpgsqlTypes.NpgsqlDbType.Integer;
+
+                        cmd.Parameters.Add(regionIdsParam);
+                        cmd.Parameters.AddWithValue("@ReferenceLatitude", locationReference.Latitude);
+                        cmd.Parameters.AddWithValue("@ReferenceLongitude", locationReference.Longitude);
+                        cmd.Parameters.AddWithValue("@PerPage", command.PerPage);
 
                         using (var reader = await cmd.ExecuteReaderAsync())
                         {
@@ -80,11 +181,11 @@ namespace SengokuProvider.API.Services.Events
             }
             catch (NpgsqlException ex)
             {
-                throw new ApplicationException("Database error occurred: ", ex);
+                throw new ApplicationException($"Database error occurred: {ex.InnerException}", ex);
             }
             catch (Exception ex)
             {
-                throw new ApplicationException("Unexpected Error Occurred: ", ex);
+                throw new ApplicationException($"Unexpected Error Occurred: {ex.StackTrace}", ex);
             }
         }
     }
